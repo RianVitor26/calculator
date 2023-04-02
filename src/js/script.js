19664a67ae81ddd86040a1c9761eec07666d5f7d
RianVitor26 <rianvitorlhe@gamil.com>
2023-04-01 22:37:28 -0300

    feat: adicionando função pra limpar o display

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,38 @@
 const display = document.querySelector('.display')
 const buttons = document.querySelectorAll('.button')
 
+let firstValue = 0
+let secondValue = 0 
+let operator = ''
+
 buttons.forEach(button => {
     button.addEventListener('click', event => {
-       const value = event.target.innerHTML
+        const value = event.target.innerHTML
         insertValueOnDisplay(value)
     })
 })
 
 function insertValueOnDisplay(value) {
+    verifyValues(value)
+    if (value === 'C') {
+        return
+    }
     display.innerHTML += value
+}   
+
+function verifyValues(value) {
+    switch (value) {
+        case 'C':
+            clearDisplay()
+            break;
+        case '': 
+
+        default:
+            break;
+    }
 }
 
+
+function clearDisplay() {
+    display.innerHTML = ''
+}
