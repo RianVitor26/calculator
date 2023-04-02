c0ff8da079a4df41c2a672631ed24ca9f7982130
RianVitor26 <rianvitorlhe@gamil.com>
2023-04-02 20:29:31 -0300

    fix: corrigindo bug na inserção de um valor

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//PEGA VISOR E BOTÕES
 const display = document.querySelector('.display')
 const buttons = document.querySelectorAll('.button')
 
@@ -8,20 +9,35 @@ buttons.forEach(button => {
     })
 })
 
-function insertValueOnDisplay(value) {
-    if (value === 'C') {
+function insertValueOnDisplay(clickedButtonValue) {
+    console.log(clickedButtonValue)
+
+    if (clickedButtonValue === 'C') {
         clearDisplay()
+        return
     }
-    else if (value === '=') {
-        display.innerHTML = eval(display.innerHTML)
+
+    if (clickedButtonValue === '=') {
+        calculateResult()
+        return
     }
-    else
-        display.innerHTML += value
+
+    display.innerHTML += clickedButtonValue
 }
-
-
 
 
 function clearDisplay() {
     display.innerHTML = ''
+}
+
+function deleteLastElement() {
+    display.innerHTML = display.innerHTML.split(0, -1)
+}
+
+function calculateResult() {
+    try {
+        display.innerHTML = eval(display.innerHTML)
+    } catch (error) {
+        display.innerHTML = ''
+    }
 }
