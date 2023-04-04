c5265d9dea09146cb9957765bcb3442f37992edd
secusergitflow20 <rianvitorlhe2@gmail.com>
2023-04-03 22:38:22 -0300

    refac: refatorando funções

--- src/js/script.js
+++ src/js/script.js
@@ -1,67 +1,115 @@
 const display = document.querySelector('.display')
 const buttons = document.querySelectorAll('.button')
 
-buttons.forEach(button => {
-    button.addEventListener('click', event => {
-        const value = event.target.innerHTML
-        insertValueOnDisplay(value)
+handleEventButtons()
+
+function handleEventButtons() {
+    buttons.forEach(button => {
+        button.addEventListener('click', event => {
+            const clickedButtonValue = event.target.innerHTML
+            insertValueOnDisplay(clickedButtonValue)
+        })
     })
-})
+}
+
+
+
+
 
 function insertValueOnDisplay(clickedButtonValue) {
 
-    let lastElementTyped = display.innerHTML[display.innerHTML.length - 1]
-    let operators = ['*', '/', '+', '.']
-    console.log(lastElementTyped)
+    if (lockOperatorAsFirstElement(clickedButtonValue)) return
 
-    if (display.innerHTML.length === 0 && operators.includes(clickedButtonValue) ) {
-        return
-    }
-   
+    if (verifyClickedButtonValue(clickedButtonValue)) return
 
-    if (clickedButtonValue === 'Del') {
-        deleteLastElement()
-        return
-     }
-
-    if (clickedButtonValue === 'x') {
-         display.innerHTML += clickedButtonValue.replace('x', '*')
-        return
-    }
-
-    if (clickedButtonValue === 'C') {
-        clearDisplay()
-        return
-    }
-
-    if (clickedButtonValue === '=') {
-        calculateResult()
-        return
-    }
-
-    if (lastElementTyped && !Number(lastElementTyped) && !Number(clickedButtonValue) && lastElementTyped  != 0 && clickedButtonValue != 0) {
-        deleteLastElement()
-    }
-
-    
+    checkDuplicateOperators(clickedButtonValue)
 
     display.innerHTML += clickedButtonValue
 }
+
+
+
+
+
+function lockOperatorAsFirstElement(clickedButtonValue) {
+    const canNotFirstElementOnDisplay = ['*', '/', '+', '.']
+
+    if (display.innerHTML.length === 0 && canNotFirstElementOnDisplay.includes(clickedButtonValue)) {
+        return true
+    }
+}
+
+
+
+
+
+const buttonActions = {
+    'Del': () => {
+        deleteLastElementOnDisplay();
+        return true;
+    },
+    'x': () => {
+        display.innerHTML += '*';
+        return true;
+    },
+    'C': () => {
+        clearDisplay();
+        return true;
+    },
+    '=': () => {
+        calculateResultOnDisplay();
+        return true;
+    }
+}
+
+function verifyClickedButtonValue(clickedButtonValue) {
+    const action = buttonActions[clickedButtonValue]
+    if (action)  return action()
+
+    return false;
+}
+
+
+
+
+
+function checkDuplicateOperators(clickedButtonValue) {
+    const lastElementOnDisplay = display.innerHTML[display.innerHTML.length - 1]
+    const isLastElementNotNumber = !Number(lastElementOnDisplay);
+    const isClickedButtonNotNumber = !Number(clickedButtonValue);
+    const isLastElementNotZero = lastElementOnDisplay !== 0;
+    const isClickedButtonNotZero = clickedButtonValue !== 0;
+
+    if (isLastElementNotNumber && isClickedButtonNotNumber && isLastElementNotZero && isClickedButtonNotZero) {
+        deleteLastElementOnDisplay();
+    }
+}
+
+
+
+
+
+function deleteLastElementOnDisplay() {
+    display.innerHTML = display.innerHTML.slice(0, -1)
+}
+
+
+
 
 
 function clearDisplay() {
     display.innerHTML = ''
 }
 
-function deleteLastElement() {
-     display.innerHTML = display.innerHTML.slice(0, -1)
-}
 
-function calculateResult() {
+
+
+
+function calculateResultOnDisplay() {
     try {
         display.innerHTML = eval(display.innerHTML)
-    } catch (error) {
+    } catch (messageError) {
         display.innerHTML = ''
-        console.log(error)
     }
 }
+
